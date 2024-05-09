da13bc1fad13548f5161db0d5466584d7d9f3a12
dipesh <dipeshmaurya152@gmail.com>
2024-05-09 12:51:35 +0530

    Login Pages

--- lib/Utils/Global.dart
+++ lib/Utils/Global.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 // import 'package:resume_app_daily_task/Component/Home/StatusBar.dart';
 
 import '../Component/Home/LoginPages/Entry_Page.dart';
-import '../Component/Home/LoginPages/GmailDrawer.dart';
+import '../Component/Home/Gmail_Drawer/GmailDrawer.dart';
 import '../Component/Home/LoginPages/Login_Page.dart';
 import '../Component/Home/Status_Bar.dart';
 
